71e8be7ec254fe0e98a3fb7990847b95d5b90e7d
0x1306a94 <0x1306a94@gmail.com>
2026-07-05 20:09:05 +0800

    reinit

--- src/code.ts
+++ src/code.ts
@@ -1,26 +1,37 @@
-import { readSelectionIr } from './ir-builder';
+// This plugin will open a window to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
 
-figma.showUI(__html__, { width: 560, height: 680 });
+// This file holds the main code for plugins. Code in this file has access to
+// the *figma document* via the figma global object.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
-type PluginMessage = { type: 'read-motion' } | { type: 'cancel' };
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
 
-function sendMotionIr() {
-  figma.ui.postMessage({ type: 'motion-ir', data: readSelectionIr(figma.currentPage.selection) });
-}
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.onmessage =  (msg: {type: string, count: number}) => {
+  // One way of distinguishing between different types of messages sent from
+  // your HTML page is to use an object with a "type" property like this.
+  if (msg.type === 'create-shapes') {
+    // This plugin creates rectangles on the screen.
+    const numberOfRectangles = msg.count;
 
-sendMotionIr();
-
-figma.on('selectionchange', () => {
-  sendMotionIr();
-});
-
-figma.ui.onmessage = (msg: PluginMessage) => {
-  if (msg.type === 'read-motion') {
-    sendMotionIr();
-    return;
+    const nodes: SceneNode[] = [];
+    for (let i = 0; i < numberOfRectangles; i++) {
+      const rect = figma.createRectangle();
+      rect.x = i * 150;
+      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
+      figma.currentPage.appendChild(rect);
+      nodes.push(rect);
+    }
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
   }
 
-  if (msg.type === 'cancel') {
-    figma.closePlugin();
-  }
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  figma.closePlugin();
 };
